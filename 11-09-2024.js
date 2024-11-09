ab478c2389ebff5bee63755f1e4e1fe996ed7f94
Mayaluisa <109621371+Mayaluisa@users.noreply.github.com>
2024-11-09 18:29:05 -0500

    5th problem

--- 11-09-2024.js
+++ 11-09-2024.js
@@ -91,3 +91,12 @@ function switchItUp(number){
 
 switchItUp = n => ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]*[n]
 
+/**
+ * Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+ */
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+     return length*width*height
+    }
+  }
