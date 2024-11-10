e147c1c7cb795ae707bfc518a1ffc16d12b7487c
Mayaluisa <109621371+Mayaluisa@users.noreply.github.com>
2024-11-10 16:04:08 -0500

    Prob 6, calc and return the next sqrt :)

--- 11-10-2024.js
+++ 11-10-2024.js
@@ -158,4 +158,29 @@ filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 function filter_list(arr) {
     return arr.filter((ele) => typeof ele === "number")
   }
-  
+
+/**
+ * You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples ( Input --> Output )
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+ */
+
+function findNextSquare(sq) {
+    let start = Math.sqrt(sq)
+    let found = 0;
+    
+    if (start !== Math.floor(start)){
+      return -1
+    } 
+    
+    let nextSq = ++start ** 2
+    
+    return nextSq
+  }
