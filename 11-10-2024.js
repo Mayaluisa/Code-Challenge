e956c69066b447618674adb6edc9d1d5d0ae58d4
Mayaluisa <109621371+Mayaluisa@users.noreply.github.com>
2024-11-10 16:10:39 -0500

    Prob 7, new string!

--- 11-10-2024.js
+++ 11-10-2024.js
@@ -184,3 +184,20 @@ function findNextSquare(sq) {
     
     return nextSq
   }
+
+/**
+ * Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+ */
+
+function doubleChar(str) {
+    let newStr = "";
+    for (let i = 0; i < str.length; i++){
+      newStr = newStr + str[i] + str[i]; 
+    }
+    return newStr
+  }
